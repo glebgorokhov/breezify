a7ea1766fc7d0ade05ed73a6111d24a55fe35420
Gleb Gorokhov <theververy@gmail.com>
2024-03-05 15:10:41 +0300

    Exclude: test data from the repo

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,141 +1,24 @@
-import {
-  extractClassesAndGenerateMap,
-  extractClassNamesFromFiles,
-  replaceClassNamesInCSS,
-} from "../src/css-functions";
-import { expectedClassNames } from "./data/expected-classes";
-import { generateObfuscatedNames } from "../src/class-name-generator";
-import { generateClassMap } from "../src/class-map";
-import { replaceClassNamesInHtml } from "../src/html-functions";
-import { replaceClassNamesInJs } from "../src/js-functions";
-import { skipEventListeners, skipLocalStorageMethods } from "../src/skip-rules";
-import { updateFileAndCompareSize } from "../src/file-functions";
-import { test, expect, describe } from "vitest";
-import { defaultOptions } from "../src/options";
+import { test, describe } from "vitest";
+import * as fs from "fs";
+import breezify from "../index";
 
 describe("generate and replace CSS class names", () => {
-  let classList: Set<string>;
-  let generatedClasses: string[];
-  let classMap: Record<string, string>;
+  const folders = fs.readdirSync("tests/data/build-files");
 
-  test("extract classes from original CSS file", () => {
-    classList = extractClassNamesFromFiles(["tests/data/style.css"], {
-      ...defaultOptions.css,
-      ignoreClassPatterns: ["interactive"],
-    });
-    expect(Array.from(classList)).toEqual(expectedClassNames);
-  });
+  folders.forEach((folder) => {
+    test(`breezify ${folder}`, async () => {
+      const buildDir = `tests/data/build-files/${folder}`;
+      const outputDir = `tests/data/output-files/${folder}`;
+      const config = `tests/data/configs/${folder}.json`;
+      const configFileExists = fs.existsSync(config);
 
-  test("replace classes in CSS file", () => {
-    generatedClasses = generateObfuscatedNames(classList, defaultOptions.css);
-    classMap = generateClassMap(classList, generatedClasses);
-
-    updateFileAndCompareSize({
-      path: "tests/data/style.css",
-      targetPath: "tests/data/style.min.css",
-      updateContent: (content) =>
-        replaceClassNamesInCSS(content, classMap, defaultOptions.css),
-    });
-  });
-
-  test("replace classes in HTML file", () => {
-    updateFileAndCompareSize({
-      path: "tests/data/index.html",
-      targetPath: "tests/data/index.min.html",
-      updateContent: (content) =>
-        replaceClassNamesInHtml(
-          content,
-          classMap,
-          defaultOptions.html,
-          defaultOptions.js,
-          defaultOptions.css,
-        ),
-    });
-  });
-
-  test("replace classes in JS file", () => {
-    updateFileAndCompareSize({
-      path: "tests/data/index.js",
-      targetPath: "tests/data/index.min.js",
-      updateContent: (content) =>
-        replaceClassNamesInJs(
-          content,
-          classMap,
-          {
-            ...defaultOptions.js,
-            minify: false,
-            skipRules: [skipEventListeners, skipLocalStorageMethods],
-          },
-          defaultOptions.css,
-        ),
-    });
-  });
-
-  test("example for readme", () => {
-    const exampleClassList = extractClassNamesFromFiles(
-      ["tests/data/example.css"],
-      defaultOptions.css,
-    );
-
-    const exampleGeneratedClasses = generateObfuscatedNames(
-      exampleClassList,
-      defaultOptions.css,
-    );
-    const exampleClassMap = generateClassMap(
-      exampleClassList,
-      exampleGeneratedClasses,
-    );
-
-    updateFileAndCompareSize({
-      path: "tests/data/example.css",
-      targetPath: "tests/data/example.min.css",
-      updateContent: (content) =>
-        replaceClassNamesInCSS(content, exampleClassMap, defaultOptions.css),
-    });
-
-    updateFileAndCompareSize({
-      path: "tests/data/example.html",
-      targetPath: "tests/data/example.min.html",
-      updateContent: (content) =>
-        replaceClassNamesInHtml(
-          content,
-          exampleClassMap,
-          defaultOptions.html,
-          defaultOptions.js,
-          defaultOptions.css,
-        ),
-    });
-  });
-
-  test("tailwind homepage", () => {
-    const cssOptions = {
-      ...defaultOptions.css,
-      ignoreClassPatterns: ["^dark$"],
-    };
-
-    const exampleClassMap = extractClassesAndGenerateMap(
-      ["tests/data/tailwind-homepage.css"],
-      cssOptions,
-    );
-
-    updateFileAndCompareSize({
-      path: "tests/data/tailwind-homepage.css",
-      targetPath: "tests/data/tailwind-homepage.min.css",
-      updateContent: (content) =>
-        replaceClassNamesInCSS(content, exampleClassMap, cssOptions),
-    });
-
-    updateFileAndCompareSize({
-      path: "tests/data/tailwind-homepage.html",
-      targetPath: "tests/data/tailwind-homepage.min.html",
-      updateContent: (content) =>
-        replaceClassNamesInHtml(
-          content,
-          exampleClassMap,
-          defaultOptions.html,
-          defaultOptions.js,
-          defaultOptions.css,
-        ),
+      await breezify({
+        config: configFileExists ? config : undefined,
+        files: {
+          buildDir,
+          outputDir,
+        },
+      });
     });
   });
 });
