85a617e4db3ac62ffd3d9e67a2d7d00af70030d0
Gleb Gorokhov <theververy@gmail.com>
2024-03-21 17:13:51 +0300

    Refactor: generate cjs versions of files

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { breezify } from "./src/breezify.js";
 import { BreezifyOptions } from "./src/options.js";
+import BreezifyWebpackPlugin from "./src/plugins/webpack.js";
 
 export default breezify;
-export { BreezifyOptions };
+export { BreezifyOptions, BreezifyWebpackPlugin };
